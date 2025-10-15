c289ce192f5b52662fdfc5641926430242157690
LiamSchaaf <Liam2.Schaaf@live.uwe.ac.uk>
2025-10-15 14:20:59 +0100

    Week #3 done

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+    [SerializeField]
+    private List<string> items = new List<string>();
+    private GameManager gamemanager;
+
+    void Start()
+    {
+        gamemanager = FindAnyObjectByType<GameManager>();
+    }
+
+    private void Update()
+    {
+        if(gamemanager.state == GameState.GAMEPLAY)
+        {
+            if (Input.GetKeyDown(KeyCode.O))
+            {
+                AddItem("Generic Item");
+            }
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                RemoveItem("Generic Item");
+            }
+        }
+        else
+        {
+
+        }
+
+    }
+
+    public void AddItem(string itemName)
+    {
+        items.Add(itemName);
+    }
+
+    public void RemoveItem(string itemName) 
+    { 
+        items.Remove(itemName); 
+    }
+
+}
